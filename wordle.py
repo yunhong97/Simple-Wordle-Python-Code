8ec94220e0e57010025996c54ae247d49be7e7af
Tan Yun Hong <97875075+yunhong97@users.noreply.github.com>
2022-01-24 22:22:19 +0800

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -10,18 +10,22 @@ answer = str(random.choice(wordle_list))
 answer_list=list(answer)
 c = []
 attempts = 0
+smol = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 
 while attempts < 7:
     attempt = str(input(f'Guess a 5 letter word! {6-attempts} guesses only :^) ' + '\n'+'All letters start with caps! Not the bald Caps from LEC KEKW' + '\n' + '\n'))
+    
     if len(attempt) != 5:
         print('This is not 5 letters you bonobo!')
         continue
     elif attempt[0] in smol:
         print ('Starts with caps sir... ')
+        continue
     elif attempt not in wordle_list:
         print ('Not a valid word!')
         continue
+    
     if attempt == answer:
             print(f'Congratulations! {attempt} is correct!')
             break
